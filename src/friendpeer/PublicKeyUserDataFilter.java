70ede24aebcc39499b5436f230d09a42f9a2dc73
Rajiv Makhijani <rajiv@blue-tech.org>
2011-03-06 07:44:52 -0800

    Added TODOs for UserDistributedMap (Incorrect serialization, map & security assumptions) Added Placeholder Filter for Public Key -> Signed User IP (PublicKeyUserDataFilter) Added Starting point for Netty ChannelHandler that takes care of message splitting when using UDP (including tentative 16-bit fixed-length header description)

---
+++ src/friendpeer/PublicKeyUserDataFilter.java
@@ -0,0 +1,13 @@
+package friendpeer;
+
+import dht.MapDataFilter;
+
+public class PublicKeyUserDataFilter implements MapDataFilter<String, String> {
+
+	@Override
+	public boolean isValid(String key, String value) {
+		// TODO: Deserialize to PublicKey / SignedMessage and validate!
+		return false;
+	}
+
+}
