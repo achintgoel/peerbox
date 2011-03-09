8afc250fe939884fdb51f9b58c233403e4474395
Vineet Pandit <vineetpandit@gmail.com>
2011-03-09 00:08:34 -0800

    run.sh again

--- run.sh
+++ run.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 export CLASSPATH="./bin/:./lib/google-gson-1.6/gson-1.6.jar:./lib/netty-3.2.4.Final/jar/netty-3.2.4.Final.jar"
 
+if [ $# -eq 0 ] ; then
+	echo "Use -m -s or -f"
+	exit
+fi
+
 args=""
 i=1
 inArgs=("$@")
@@ -16,4 +21,6 @@ elif [ $1 = "-s" ] ; then
 	java org.peerbox.testlets.DumbCLI $args
 elif [ $1 = "-f" ] ; then
 	java org.peerbox.testlets.FileShareTest $args
+else
+	echo "Use -m -s or -f"
 fi
