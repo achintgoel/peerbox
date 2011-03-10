48678aab9d45c847b4879c52f890fc8f3ae87c19
Achint Goel <achintg12@gmail.com>
2011-03-09 22:38:04 -0800

    added code to FriendCLIHandler

--- src/org/peerbox/demo/cli/FriendCLIHandler.java
+++ src/org/peerbox/demo/cli/FriendCLIHandler.java
@@ -1,11 +1,84 @@
 package org.peerbox.demo.cli;
 
-public class FriendCLIHandler implements CLIHandler{
+import java.math.BigInteger;
+import java.security.KeyFactory;
+import java.security.NoSuchAlgorithmException;
+import java.security.PublicKey;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.X509EncodedKeySpec;
 
+import org.peerbox.friend.FriendManager;
+
+public class FriendCLIHandler implements CLIHandler{
+	
+	
+	protected FriendManager friend_manager;
+	public FriendCLIHandler(FriendManager manager) {
+		this.friend_manager = manager;
+	}
 	@Override
 	public void handleCommand(String[] args, ExtendableCLI cli) {
 		// TODO Auto-generated method stub
+		if(args.length < 2) {
+			help();
+			return;
+		}
+		String function = args[1];
+		if(function.equalsIgnoreCase("add")) {
+			
+			if(args.length != 4) {
+				help();
+				return;
+			}
+			String alias = args[2];
+			BigInteger key = new BigInteger(args[3]);
+			X509EncodedKeySpec pubKeySpec = new X509EncodedKeySpec(key.toByteArray());
+			KeyFactory keyFactory;
+			PublicKey publicKey;
+			try {
+				keyFactory = KeyFactory.getInstance("DSA");
+				publicKey = keyFactory.generatePublic(pubKeySpec);
+				friend_manager.createFriend(alias, null, publicKey);
+				cli.out().println("Added "+alias+" to buddy list");
+			} catch (NoSuchAlgorithmException e) {
+				// TODO Auto-generated catch block
+				//e.printStackTrace();
+				cli.out().println("Unable to generate key based on algorithm specified");
+			} catch (InvalidKeySpecException e) {
+				// TODO Auto-generated catch block
+				//e.printStackTrace();
+				cli.out().println("Generated key is invalid");
+			}
+			
+		}
+		else if(function.equalsIgnoreCase("delete")) {
+			if(args.length != 3) {
+				help();
+				return;
+			}
+			String alias = args[2];
+			if(friend_manager.getFriend(alias) != null) {
+				friend_manager.removeFriend(alias);
+			}
+			else {
+				cli.out().println("The friend "+alias+" does not exist");
+			}
+			
+		}
+		else if(function.equalsIgnoreCase("list")) {
+			if(args.length > 2) {
+				help();
+				return;
+			}
+			friend_manager.printBuddyList();
+		}
+		else {
+			help();
+		}
 		
+	}
+	public void help(){
+		System.out.println("Illegal Command");
 	}
 
 }
