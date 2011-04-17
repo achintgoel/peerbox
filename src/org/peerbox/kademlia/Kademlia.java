70356957eea3ad4270091f0e7f3a7779cdfe56a3
Rajiv Makhijani <rajiv@blue-tech.org>
2011-04-17 16:42:43 -0700

    add Kademlia interface

---
+++ src/org/peerbox/kademlia/Kademlia.java
@@ -0,0 +1,46 @@
+package org.peerbox.kademlia;
+
+import java.net.URI;
+import java.util.List;
+
+import org.peerbox.dht.DistributedMap;
+import org.peerbox.dht.MapDataFilter;
+import org.peerbox.kademlia.messages.FindNodeResponse;
+import org.peerbox.kademlia.messages.FindValueResponse;
+import org.peerbox.kademlia.messages.PingResponse;
+import org.peerbox.kademlia.messages.StoreResponse;
+
+public interface Kademlia {
+
+	void registerDataFilter(String primaryKey,
+			MapDataFilter<String, String> dataFilter);
+
+	Identifier getLocalNodeIdentifier();
+
+	Configuration getConfiguration();
+
+	Buckets getBuckets();
+
+	DistributedMap<Key, String> getPrimaryDHT();
+
+	DistributedMap<String, String> getSingleMap(String mapKey);
+
+	void findNode(Identifier targetNodeId, boolean stopOnFound,
+			ResponseListener<FindNodeResponse> responseListener);
+
+	void findNode(Identifier targetNodeId,
+			ResponseListener<FindNodeResponse> responseListener);
+
+	void findValue(Key targetKey,
+			ResponseListener<FindValueResponse> responseListener);
+
+	void storeValue(Key key, String value, boolean publish,
+			ResponseListener<StoreResponse> responseListener);
+
+	boolean storeValueLocal(Key key, String value);
+
+	void ping(Node targetNode, ResponseListener<PingResponse> responseListener);
+
+	void bootstrap(List<URI> friends, BootstrapListener bootstrapListener);
+	
+}
